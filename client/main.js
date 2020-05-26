812b399a8ffed4dcd24c705040e7a429a0235c79
mateuszdabski <mateuszdabski96@gmail.com>
2020-05-26 12:33:16 +0200

    [added] adding and taking points from players

--- client/main.js
+++ client/main.js
@@ -19,5 +19,13 @@ Template.leaderboard.events({
   'click .player': function(){
     var playerId = this._id;
     Session.set('selectedPlayer', playerId);
-  }   
+  },
+  'click .increment': function(){
+    var selectedPlayer = Session.get('selectedPlayer');
+    PlayersList.update(selectedPlayer, {$inc: {score: 1}});
+  },
+  'click .decrement': function(){
+    var selectedPlayer = Session.get('selectedPlayer');
+    PlayersList.update(selectedPlayer, {$inc: {score: -1}});
+  }
 });
