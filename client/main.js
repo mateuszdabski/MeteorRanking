c41b19b3ef30ac8210c65b61e8bfdcc9d6471bdc
mateuszdabski <mateuszdabski96@gmail.com>
2020-05-26 12:26:10 +0200

    [added] highlighting selected player

--- client/main.js
+++ client/main.js
@@ -4,7 +4,20 @@ PlayersList = new Mongo.Collection('players');
 
 Template.leaderboard.helpers({
   'player': function(){
-      return PlayersList.find()
+      return PlayersList.find();
+  },
+  'selectedClass': function(){
+    var playerId = this._id;
+    var selectedPlayer = Session.get('selectedPlayer');
+    if(playerId == selectedPlayer){
+      return "selected"
+    }
   }
 });
 
+Template.leaderboard.events({
+  'click .player': function(){
+    var playerId = this._id;
+    Session.set('selectedPlayer', playerId);
+  }   
+});
